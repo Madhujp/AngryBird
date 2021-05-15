472f2bf73252b43b24c85d8be525c4e99d1c30b7
Madhujp <madhujp2015@gmail.com>
2021-05-15 06:21:05 +0530

    Add files via upload

--- Log.js
+++ Log.js
@@ -2,7 +2,7 @@ class Log extends Baseclass{
     constructor(x, y, height, angle) {
      super(x,y,20,height,angle);
      Matter.Body.setAngle(this.body,angle);
-     this.image=loadImage("wood2.png");
+     this.image=loadImage("/wood2.png");
     }
   };
   
